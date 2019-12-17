5d899625e27872f0988c38c3723e0595aaea4788
Kitikorn Khayankan <kitikorn.k@scale360solutions.com>
2019-12-17 20:27:21 +0700

    fix

--- app.js
+++ app.js
@@ -14,29 +14,55 @@ app.post('/webhook', (req, res) => {
 app.listen(port)
 
 
-reply = (reply_token) => {
-  let header = {
+// reply = (reply_token) => {
+//   let header = {
+//     'Content-Type': 'application/json',
+//     'Authorization': 'Bearer {4n+lqCft0dHfz2Cu3oI5FDc+W1cM8VNQHfiHn2eoRK14dshoX+o8HkLJSl95IYc/LQ0O/mZTcfytgDpNIdwpKTFoL3pcNwMn0Unoa37lUOAsyPdzj/El7aVSp2WQ0J2WvzwHzo5ElpS+WN0ZAe/9NgdB04t89/1O/w1cDnyilFU=}'
+//   }
+
+//   let body = JSON.stringify({
+//     replyToken: reply_token,
+//     messages: [{
+//         type: 'text',
+//         text: 'Hello'
+//     },
+//     {
+//         type: 'text',
+//         text: 'How are you?'
+//     }]
+//   })
+
+//   request.post({
+//     url: 'https://api.line.me/v2/bot/message/reply',
+//     header: header,
+//     body: body,
+//   }, (err, res, body) => {
+//       console.log('status = ' + res.statusCode)
+//   })
+// }
+
+function reply(reply_token) {
+  let headers = {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer {4n+lqCft0dHfz2Cu3oI5FDc+W1cM8VNQHfiHn2eoRK14dshoX+o8HkLJSl95IYc/LQ0O/mZTcfytgDpNIdwpKTFoL3pcNwMn0Unoa37lUOAsyPdzj/El7aVSp2WQ0J2WvzwHzo5ElpS+WN0ZAe/9NgdB04t89/1O/w1cDnyilFU=}'
   }
-
   let body = JSON.stringify({
     replyToken: reply_token,
     messages: [{
         type: 'text',
         text: 'Hello'
-    },
-    {
+      },
+      {
         type: 'text',
         text: 'How are you?'
-    }]
+      }
+    ]
   })
-
   request.post({
     url: 'https://api.line.me/v2/bot/message/reply',
-    header: header,
-    body: body,
+    headers: headers,
+    body: body
   }, (err, res, body) => {
-      console.log('status = ' + res.statusCode)
-  })
+    console.log('status = ' + res.statusCode);
+  });
 }
