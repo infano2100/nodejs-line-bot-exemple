0acd18f38af25e5879130a8496e6bed3c0f849dd
Kitikorn Khayankan <kitikorn.k@scale360solutions.com>
2019-12-17 07:49:29 +0700

    add function send message bot

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
 const app = express()
 const port = process.env.PORT || 4000
-app.post('/webhook', (req, res) => res.sendStatus(200))
-app.listen(port, () => {
-  console.log("App is running on port " + port);
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
+  let { reply_token } = req.body.events[0]
+  reply(reply_token)
+  res.sendStatus(200)
 })
+app.listen(port)
+reply = (reply_token) => {
+  let headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer {punpuPSfr4UXjWEP4rUVZHMf/vhNHMjnXlOTPYRtB7pl33GkfIUtUS8MyWkfIG8PLQ0O/mZTcfytgDpNIdwpKTFoL3pcNwMn0Unoa37lUOA35kPEvirxg93jRDAoawsK1cvGVickdYkqAMXW63WCEAdB04t89/1O/w1cDnyilFU=}'
+  }
+  let body = JSON.stringify({
+    replyToken: reply_token,
+    messages: [{
+        type: 'text',
+        text: 'Hello'
+    },
+    {
+        type: 'text',
+        text: 'How are you?'
+    }]
+  })
+  request.post({
+    url: 'https://api.line.me/v2/bot/message/reply',
+    headers: headers,
+    body: body
+  }, (err, res, body) => {
+      console.log('status = ' + res.statusCode);
+  })
+}
