d3efeb73842acd92390e0de4799a8ef456770957
Kitikorn Khayankan <kitikorn.k@scale360solutions.com>
2019-12-17 21:06:23 +0700

    fix

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 
 const port = process.env.PORT || 4000
 const url = 'https://api.line.me/v2/bot/message/reply'
-const token = '4n+lqCft0dHfz2Cu3oI5FDc+W1cM8VNQHfiHn2eoRK14dshoX+o8HkLJSl95IYc/LQ0O/mZTcfytgDpNIdwpKTFoL3pcNwMn0Unoa37lUOAsyPdzj/El7aVSp2WQ0J2WvzwHzo5ElpS+WN0ZAe/9NgdB04t89/1O/w1cDnyilFU='
+const token = '' // Channel line access token 
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -18,14 +18,14 @@ app.post('/webhook', (req, res) => {
 app.listen(port)
 
 
-reply = (reply_token, text) => {
+reply = (replyToken, text) => {
   let headers = {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer {'+token+'}',
   }
 
   let body = JSON.stringify({
-    replyToken: reply_token,
+    replyToken,
     messages: [{
       type: 'text',
       text: 'Hello'
@@ -40,7 +40,7 @@ reply = (reply_token, text) => {
     url,
     headers,
     body,
-  }, (err, res, body) => {
+  }, (res) => {
       console.log('status = ' + res.statusCode)
   })
 }
