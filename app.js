02cd91570709538146caee62088bb4c0c088570f
Kitikorn Khayankan <kitikorn.k@scale360solutions.com>
2019-12-17 20:22:57 +0700

    fix

--- app.js
+++ app.js
@@ -3,35 +3,40 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 const port = process.env.PORT || 4000
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
-    let reply_token = req.body.events[0].replyToken
-    reply(reply_token)
-    res.sendStatus(200)
+  let { replyToken } = req.body.events[0]
+  reply(replyToken)
+  res.sendStatus(200)
 })
 app.listen(port)
-function reply(reply_token) {
-    let headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer {4n+lqCft0dHfz2Cu3oI5FDc+W1cM8VNQHfiHn2eoRK14dshoX+o8HkLJSl95IYc/LQ0O/mZTcfytgDpNIdwpKTFoL3pcNwMn0Unoa37lUOAsyPdzj/El7aVSp2WQ0J2WvzwHzo5ElpS+WN0ZAe/9NgdB04t89/1O/w1cDnyilFU=}'
-    }
-    let body = JSON.stringify({
-        replyToken: reply_token,
-        messages: [{
-            type: 'text',
-            text: 'Hello'
-        },
-        {
-            type: 'text',
-            text: 'How are you?'
-        }]
-    })
-    request.post({
-        url: 'https://api.line.me/v2/bot/message/reply',
-        headers: headers,
-        body: body
-    }, (err, res, body) => {
-        console.log('status = ' + res.statusCode);
-    });
+
+const url = 'https://api.line.me/v2/bot/message/reply'
+function reply(reply_token = '') {
+  let header = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer {4n+lqCft0dHfz2Cu3oI5FDc+W1cM8VNQHfiHn2eoRK14dshoX+o8HkLJSl95IYc/LQ0O/mZTcfytgDpNIdwpKTFoL3pcNwMn0Unoa37lUOAsyPdzj/El7aVSp2WQ0J2WvzwHzo5ElpS+WN0ZAe/9NgdB04t89/1O/w1cDnyilFU=}'
+  }
+
+  let body = JSON.stringify({
+    replyToken: reply_token,
+    messages: [{
+        type: 'text',
+        text: 'Hello'
+    },
+    {
+        type: 'text',
+        text: 'How are you?'
+    }]
+  })
+
+  request.post({
+    url,
+    header,
+    body,
+  }, (err, res, body) => {
+      console.log('status = ' + res.statusCode)
+  })
 }
