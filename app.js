339498f0566aa642474e715ea52a8705168635b4
Kitikorn Khayankan <kitikorn.k@scale360solutions.com>
2019-12-17 21:08:30 +0700

    fix

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const app = express()
 const port = process.env.PORT || 4000
 const url = 'https://api.line.me/v2/bot/message/reply'
 const token = '4n+lqCft0dHfz2Cu3oI5FDc+W1cM8VNQHfiHn2eoRK14dshoX+o8HkLJSl95IYc/LQ0O/mZTcfytgDpNIdwpKTFoL3pcNwMn0Unoa37lUOAsyPdzj/El7aVSp2WQ0J2WvzwHzo5ElpS+WN0ZAe/9NgdB04t89/1O/w1cDnyilFU=' // Channel line access token 
+const messages = [{
+    type: 'text',
+    text: 'Hello'
+  },
+  {
+    type: 'text',
+    text: 'How are you?'
+}]
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -23,19 +31,7 @@ reply = (replyToken, text) => {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer {'+token+'}',
   }
-
-  let body = JSON.stringify({
-    replyToken,
-    messages: [{
-      type: 'text',
-      text: 'Hello'
-      },
-      {
-      type: 'text',
-      text: 'How are you?'
-    }]
-  })
-
+  let body = JSON.stringify({ replyToken, messages })
   request.post({
     url,
     headers,
